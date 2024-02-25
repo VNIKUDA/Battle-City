f6695c47d8cfb34d693e4697726186f3767cec94
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-02-25 16:35:40 +0200

    Додано класс кнопки

--- graphic_elements.py
+++ graphic_elements.py
@@ -2,9 +2,29 @@
 import pygame
 from abc import ABC, abstractmethod
 
-# Класс постачальника (абстрактний класс для всіх графічних елементів)
-class Subject(ABC):
-    # Абстрактний метод сповіщає всіх спостеріначів та виконує команду пов'язану з ними
+# Класс графічного елемента (абстрактний класс для всіх графічних елементів)
+class GraphElement(ABC):
+    # Абстрактний метод який додає функцію спостерігача
+    @abstractmethod
+    def add_observers_function(self, function):
+        pass
+
+    # Абстрактний метод виконує команди спостерігачів 
     @abstractmethod
     def notify_observers(self):
         pass
+
+# Класс кнопки
+class Button(GraphElement):
+    # Cтворення об'єкта Button
+    def __init__(self):
+        self.observers_functions = []
+
+    # Додавання функції яка буде запущена при натискані на кнопку
+    def add_observers_function(self, function):
+        self.observers_functions.append(function)
+
+    # Запуск функцій в self.observers_functions
+    def notify_observers(self):
+        for function in self.observers_functions:
+            function()
