aa550d93753e1b9f645ad4b36021b4bd116631ac
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-02-25 13:27:22 +0200

    Added graphic_elements file

--- graphic_elements.py
+++ graphic_elements.py
@@ -2,7 +2,9 @@
 import pygame
 from abc import ABC, abstractmethod
 
+# Класс постачальника (абстрактний класс для всіх графічних елементів)
 class Subject(ABC):
+    # Абстрактний метод сповіщає всіх спостеріначів та виконує команду пов'язану з ними
     @abstractmethod
     def notify_observers(self):
         pass
