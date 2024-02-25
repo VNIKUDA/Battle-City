30915b558035cc59e40a47298ea16b47c6a4d323
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-02-25 21:23:57 +0200

    Додано налаштування текстури кнопки

--- interface_elements.py
+++ interface_elements.py
@@ -14,11 +14,20 @@ class GraphElement(ABC):
     def notify_observers(self):
         pass
 
+
 # Класс кнопки
 class Button(GraphElement):
     # Cтворення об'єкта Button
-    def __init__(self):
+    def __init__(self, pos, filename):
+        # Список функцій спостерігачів
         self.observers_functions = []
+
+        # Текстура кнопки
+        self.image = pygame.image.load(filename)
+        
+        # Створення rect для відстеження натискання на цю кнопку та встановлення позиції rect-а
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
 
     # Додавання функції яка буде запущена при натискані на кнопку
     def add_observers_function(self, function):
